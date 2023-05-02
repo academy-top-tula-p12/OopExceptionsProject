14073c533c2d029a7876a17ff4a8ad2b0adffe8d
maxefimov <efimov.top.academy@gmail.com>
2023-05-02 21:19:43 +0300

    Добавьте файлы проекта.

---
+++ OopExceptionsProject/OopExceptionsProject.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+enum class ExcType
+{
+    IntExc,
+    FloatExp,
+    CharExp
+};
+
+void ExcThrow(ExcType type)
+{
+    switch (type)
+    {
+    case ExcType::IntExc:
+        throw 100L;
+    case ExcType::FloatExp:
+        throw 123.45;
+    case ExcType::CharExp:
+        throw "ERROR";
+    default:
+        throw true;
+    }
+}
+
+int main()
+{
+    /*
+    try
+    {
+        ExcThrow(ExcType::IntExc);
+    }
+    catch (int e)
+    {
+        std::cout << "Integer value exception " << e << "\n";
+    }
+    catch (float e)
+    {
+        std::cout << "Float value exception " << e << "\n";
+    }
+    catch (const char* e)
+    {
+        std::cout << "C-style string value exception " << e << "\n";
+    }
+    catch (...)
+    {
+        std::cout << "Any type value exception " << "\n";
+    }
+
+    std::cout << "other code...\n";
+    */
+    try
+    {
+        int n;
+        std::cin >> n;
+
+        if (n == 0) throw 1;
+
+        std::cout << 10 / n;
+    }
+    catch (...)
+    {
+        std::cout << "Divide by zero\n";
+    }
+    std::cout << "other code...\n";
+}
